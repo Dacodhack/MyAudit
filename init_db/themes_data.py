a05999d5a171a3871dc4626f00b27cd78d8900a0
Dacodhack <43749336+Dacodhack@users.noreply.github.com>
2024-10-17 17:36:29 +0200

    Delete init_db/themes_data.py
    delete old file

--- init_db/themes_data.py
+++
@@ -1,8 +0,0 @@
-# init_db/themes_data.py
-
-def get_data():
-    return [
-        {"model": "Themes", "data": {"id_theme": 1, "theme": "Audit ISO"}},
-        {"model": "Themes", "data": {"id_theme": 2, "theme": "Audit LPM"}},
-        {"model": "Themes", "data": {"id_theme": 3, "theme": "EBIOS RM"}}
-    ]
